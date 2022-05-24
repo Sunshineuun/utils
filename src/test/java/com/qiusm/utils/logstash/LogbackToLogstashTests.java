a7236b3c7e68f2b6d2df0a9de6b64bbdfdd4a7a8
qiushengming <qiushengming@aliyun.com>
2022-05-24 19:09:02 +0800

    logback to logstash tests

--- src/test/java/com/qiusm/utils/logstash/LogbackToLogstashTests.java
+++ src/test/java/com/qiusm/utils/logstash/LogbackToLogstashTests.java
@@ -11,10 +11,21 @@ import org.junit.jupiter.api.Test;
  */
 @Slf4j
 public class LogbackToLogstashTests extends UtilsApplicationTests {
+    /**
+     * 注意：如果希望 logback 输入堆栈信息的话，请按<code>log.error("{}", e.getMessage(), e);</code>的形式输入ERROR日志，第三个参数是Exception对象
+     */
     @Test
     void toLogstash() {
-        for (int i = 0; i < 10; i++) {
-            log.info("测试日志输出：{}", i);
+        try {
+            for (int i = 0; i < 10; i++) {
+                if (i == 8) {
+                    throw new NullPointerException("test null");
+                } else {
+                    log.info("测试日志输出：{}", i);
+                }
+            }
+        } catch (Exception e) {
+            log.error("{}", e.getMessage(), e);
         }
     }
 }
